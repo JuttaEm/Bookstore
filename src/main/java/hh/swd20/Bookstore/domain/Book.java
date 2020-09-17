6d54a4b5e6d0882a9c3dd7a22c8c6bf54b7f60ad
JuttaEm <jutta.ruuskanen@gmail.com>
2020-09-17 08:47:31 +0300

    aloitus

---
+++ src/main/java/hh/swd20/Bookstore/domain/Book.java
@@ -0,0 +1,14 @@
+package hh.swd20.Bookstore.domain;
+
+// Add a new model class called Book which contains attributes: title, author, year, isbn, price
+
+public class Book {
+	
+	private String title;
+	private String author;
+	private String year;
+	private String isbn;
+	private int price;
+	
+
+}
