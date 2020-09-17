ccfc8eec105e5018667ce3440cf19f253543321e
JuttaEm <jutta.ruuskanen@gmail.com>
2020-09-17 09:13:34 +0300

    Thymeleaf-template created and Controller-annotation added

--- src/main/java/hh/swd20/Bookstore/web/BookController.java
+++ src/main/java/hh/swd20/Bookstore/web/BookController.java
@@ -1,26 +1,18 @@
 package hh.swd20.Bookstore.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import hh.swd20.Bookstore.domain.Book;
 
 
+@Controller
 public class BookController {
 	
 	@GetMapping("/index")
-	public String listBooks(Model model) {
+	public String sayWelcome() {
 			
-		List<Book> books = new ArrayList<>();
-		books.add(new Book()); 
-		books.add(new Book());
-		books.add(new Book());
-		
-		model.addAttribute("books", books); 
-		
-		return "booklist";
+		return "index";
 	}
 }
