292d3a18757cae0d71ad095f43827d7e8d2be311
JuttaEm <jutta.ruuskanen@gmail.com>
2020-09-22 16:28:51 +0300

    Updated BookstoreApplication with logger

--- src/main/java/hh/swd20/Bookstore/BookstoreApplication.java
+++ src/main/java/hh/swd20/Bookstore/BookstoreApplication.java
@@ -1,5 +1,7 @@
 package hh.swd20.Bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +12,8 @@ import hh.swd20.Bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
@@ -18,14 +22,21 @@ public class BookstoreApplication {
 	@Bean
 	public CommandLineRunner demo(BookRepository bookRepository) {
 	return (args) -> {
-			
+		
+		log.info("save demo data of books");
+		
 		Book book1 = new Book("Rise of the Robots", "Martin Ford", "2016", "9780465097531", 16.90);
 		Book book2 = new Book("Veljeni Leijonamieli", "Astrid Lindgren", "1973", "9789510432761", 21.95);
 		Book book3 = new Book("Carrie", "Stephen King", "2011", "9781444720693", 11.50);
 		
 		bookRepository.save(book1);
 		bookRepository.save(book2);
-		bookRepository.save(book3);		
+		bookRepository.save(book3);	
+		
+		log.info("fetch all books");
+		for (Book book : bookRepository.findAll()) {
+			log.info(book.toString());
+		}
 		
 	};
 	}
