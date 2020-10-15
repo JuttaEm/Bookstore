f98749d6f63309b43a1299b6e23843bb6530dbf7
JuttaEm <jutta.ruuskanen@gmail.com>
2020-10-15 10:41:50 +0300

    First security features added

--- src/main/java/hh/swd20/Bookstore/web/BookController.java
+++ src/main/java/hh/swd20/Bookstore/web/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -28,12 +29,18 @@ public class BookController {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
-		/*
+	// Login    
+	@RequestMapping(value="/login")
+	    public String login() {	
+	        return "login";
+	    }	
+	
+		
 		@GetMapping("/booklist")
 		public String getAllBooks(Model model) {			
 			model.addAttribute("books", bookRepository.findAll());			
 			return "booklist";
-		}*/
+		}
 		
 		//RESTful service to get all books
 		@RequestMapping(value="/books", method = RequestMethod.GET)
@@ -61,6 +68,7 @@ public class BookController {
 			return "redirect:booklist";
 		}
 		
+		@PreAuthorize("hasRole('ADMIN')")
 		@RequestMapping(value="/delete/{id}", method = RequestMethod.GET)
 		public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 			bookRepository.deleteById(bookId);
